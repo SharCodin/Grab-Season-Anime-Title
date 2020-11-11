620663dff50634ececd89aa2b72afa8dda998645
SSharWorks <SharCoding@gmail.com>
2020-11-11 20:11:44 +0400

    refactor code, fixed code

--- get_new_season_anime_names.py
+++ get_new_season_anime_names.py
@@ -1,25 +1,13 @@
-from os import startfile
 import requests
 from bs4 import BeautifulSoup
-from timeit import default_timer as Timer
-
-startTimer = Timer()
-
-
 
 URL = 'https://myanimelist.net/anime/season'
 
 page = requests.get(URL).text
 soup = BeautifulSoup(page, 'lxml')
 
-with open("new_anime.txt", "w", encoding='utf-8') as f:
-
-    for i in soup.find_all('p', {"class": "title-text"}):
-        name = str(i.text).strip()
+with open("new_anime.txt", "a", encoding='utf-8') as f:
+    for i in soup.find_all('div', {"class": "title"}):
+        tag = i.find('a', class_="link-title")
+        name = tag.text.strip()
         f.write(name + '\n')
-
-
-# ====================  ====================
-stopTimer = Timer()
-print(str(round(stopTimer - startTimer, 2)))
-print('-' * 20)
